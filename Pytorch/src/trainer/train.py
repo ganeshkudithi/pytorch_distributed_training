c82e7a902738fcedc02b353bdda8a3621712ef4c
VaibhavBarawkar08 <115580931+VaibhavBarawkar08@users.noreply.github.com>
2026-06-19 15:11:28 +0530

    Update train.py

--- Pytorch/src/trainer/train.py
+++ Pytorch/src/trainer/train.py
@@ -1,1 +1,31 @@
 
+from src.distributed.ddp import setup_ddp, wrap_ddp
+
+local_rank = setup_ddp()
+
+model = load_model(config)
+
+model = apply_liger(model, config)
+
+model = apply_compile(model, config)
+
+model = wrap_ddp(
+    model,
+    local_rank,
+    config,
+)
+
+from src.distributed.fsdp import setup_fsdp, wrap_fsdp
+
+local_rank = setup_fsdp()
+
+model = load_model(config)
+
+model = apply_liger(model, config)
+
+model = apply_compile(model, config)
+
+model = wrap_fsdp(
+    model,
+    config,
+)
